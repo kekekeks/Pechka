419d9ba9169ad193ce2be91b22b68f634c7a9c3f
Kirill Poletaev <mr.dead.toast@gmail.com>
2023-03-21 19:25:05 +0300

    Update template via PechkaBuilder

--- samples/MyWebApp/Program.cs
+++ samples/MyWebApp/Program.cs
@@ -1,4 +1,30 @@
 using MyWebApp;
 using Pechka.AspNet;
 
-return PechkaProgram.Main<Startup>(args);
+
+void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+{
+    services.AddControllers();
+    services.AddDbContextManager((dp, c) => new MyDbContextManager(dp, c));
+}
+
+void Configure(WebHostBuilderContext ctx, IApplicationBuilder app)
+{
+    app.UseRouting();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+}
+
+void CustomizeHost(IHostBuilder hostBuilder, IConfiguration args)
+{
+    
+}
+
+PechkaProgramBuilder<Program>
+    .Create(args)
+    .ConfigureServices(ConfigureServices)
+    .ConfigureApp(Configure)
+    .CustomizeHost(CustomizeHost)
+    .Run();
